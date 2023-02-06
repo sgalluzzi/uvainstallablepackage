b68d4aba6a357d087640a303d0fcd80ea365ff54
sgalluzzi <89170735+sgalluzzi@users.noreply.github.com>
2023-02-06 18:28:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup, find_packages
 setup(name='shared',
       version='1.0.1',
       description='This package has shared components.',
-      author='Efrain Olivares',
-      author_email='efrain.olivares@gmail.com',
+      author='Efrain Olivares edited by Seth Galluzzi',
+      author_email='efrain.olivares@gmail.com, vzw6yk@virginia.edu',
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       license='LICENSE.txt',
     )
