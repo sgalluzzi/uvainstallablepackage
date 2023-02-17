64911b85440158384d6d826c9663f3b59bc86188
sgalluzzi <89170735+sgalluzzi@users.noreply.github.com>
2023-02-17 14:28:25 -0500

    Update test_shared.py

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -6,3 +6,27 @@ def test_clean_string():
     test_str = " This! is      a ,test string  "
 
     assert "This is a test string" == sh.clean_string(test_str), "String <{}> not cleaned as expected".format(test_str)
+
+ 
+#B
+def test_addition():
+    result = 5 + 4
+    assert result == 9
+
+@pytest.mark.xfail    
+def test_subtraction():
+    result = 5 - 2
+    assert result == 4
+    
+#C    
+@pytest.mark.skip(reason="Skip the bad math!")    
+def test_multiplication():
+    result = 5 * 2
+    assert result == 11
+
+#D
+@pytest.mark.skipif(sys.platform == "darwin",reason="He has evolved past division.")
+def test_division():
+    print(sys.platform)
+    result = 20 / 4
+    assert result == 6
