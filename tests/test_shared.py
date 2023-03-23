1482855803ebc4bdf6d2bc40bf8b14a6d9f5e7a8
sgalluzzi <89170735+sgalluzzi@users.noreply.github.com>
2023-03-23 12:54:27 -0400

    Update test_shared.py

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -16,17 +16,17 @@ def test_addition():
 @pytest.mark.xfail    
 def test_subtraction():
     result = 5 - 2
-    assert result == 4
+    assert result == 3
     
 #C    
 @pytest.mark.skip(reason="Skip the bad math!")    
 def test_multiplication():
     result = 5 * 2
-    assert result == 11
+    assert result == 10
 
 #D
 @pytest.mark.skipif(sys.platform == "darwin",reason="He has evolved past division.")
 def test_division():
     print(sys.platform)
     result = 20 / 4
-    assert result == 6
+    assert result == 5
